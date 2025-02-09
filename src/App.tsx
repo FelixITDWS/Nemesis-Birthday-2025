025713b36544e4a9f0e0bea05916a5272deb8879
Felix <twchenfeng@gmail.com>
2025-02-09 22:25:01 +0800

    add moving background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import FlipCard from "./FlipCard";
-import frontImage from "./assets/front.png"; // 放置圖片的路徑
-import backImage from "./assets/back.png"; // 放置圖片的路徑
+import frontImage from "./assets/front.png";
+import backImage from "./assets/back.png";
+import backgroundImage from "./assets/Nemo.png"
+import "./index.css"; // 確保載入 CSS
 
 const App: React.FC = () => {
   return (
-    <div
+    <div>
+      <img src={backgroundImage} alt="Background" className="moving-background" />
+
+      <div
       style={{
         display: "flex",
         flexDirection: "column",
@@ -14,13 +19,15 @@ const App: React.FC = () => {
         width: "100vw",
         height: "100vh",
         textAlign: "center",
-        gap: "20px", // 控制 h1 與 FlipCard 的距離
+        position: "relative",
       }}
-    >
-      <h1 style={{ marginTop: "50px", color: "black" }}>祝捏摸 2025 生日快樂</h1>
-      <p style={{ color: "black" }}>... 點按翻開卡片 ...</p>
-      <FlipCard frontImage={frontImage} backImage={backImage} />
+      >
+        <h1 style={{ marginTop: "50px", color: "black" }}>祝捏摸 2025 生日快樂</h1>
+        <p style={{ color: "black" }}>... 點按翻開卡片 ...</p>
+        <FlipCard frontImage={frontImage} backImage={backImage} />
+      </div>
     </div>
+    
   );
 };
 
